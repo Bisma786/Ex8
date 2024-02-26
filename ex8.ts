768a65ec293501046235ff929f013e7261bf632e
Bisma786 <159830145+Bisma786@users.noreply.github.com>
2024-02-26 21:20:20 +0500

    Initial commit

---
+++ ex8.ts
@@ -0,0 +1,11 @@
+// Q:8 You should create four lines that look like this:
+// console.log(5 + 3)
+console.log("_".repeat(100))
+console.log(5+3)
+console.log("=".repeat(100))
+console.log(16-8)
+console.log("-".repeat(100))
+console.log(4*2)
+console.log("_".repeat(100))
+console.log(16/2)
+console.log("=".repeat(100))
